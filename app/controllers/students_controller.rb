546ffca422d5f3ddb22b56bda4877009815a1a2d
Alvee Akand <alvee.akand@outlook.com>
2021-07-01 21:18:33 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
@@ -8,4 +9,21 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    student = params[:student]
+    first_name = student[:first_name]
+    last_name = student[:last_name]
+    Student.create(first_name: first_name, last_name: last_name)
+    redirect_to(students_path)
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+  
 end
